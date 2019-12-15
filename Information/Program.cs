f46d4c429ca06874b31829122d7ab44a497f77a4
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-12-15 22:32:39 +0300

    Information

---
+++ Information/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя:");
+            string name = Console.ReadLine();
+            Console.Write("Ваш возраст:");
+            string age = Console.ReadLine();
+            int a = Convert.ToInt32(age); 
+            Console.Write("Где Вы живете:");
+            string live = Console.ReadLine();
+            Console.WriteLine($"Ну что ж {name}, мы получили все данные, которые нужны были.Теперь готовьтесь! За Вами выехали!");
+        }
+    }
+}
